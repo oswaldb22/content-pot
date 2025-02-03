603434116bb57b7554c2bf751e9171a71b1c8754
Oswald BLASSOU <oswaldblassou@yahoo.fr>
2025-02-03 22:40:57 +0100

    feat: added data page to export all data

---
+++ src/pages/Data.tsx
@@ -0,0 +1,39 @@
+import { Article } from "@/components/ArticleList";
+import { Preferences } from "@/hooks/usePreferences";
+import { useEffect, useState } from "react";
+
+interface StoredData {
+  articles: Article[];
+  preferences: {
+    current: Preferences;
+    next: Preferences;
+  };
+}
+
+const Data = () => {
+  const [data, setData] = useState<StoredData>({
+    articles: [],
+    preferences: {
+      current: null,
+      next: null,
+    },
+  });
+
+  useEffect(() => {
+    const articles = localStorage.getItem("articles");
+    const currentPreferences = localStorage.getItem("preferences");
+    const nextPreferences = localStorage.getItem("next-preferences");
+
+    setData({
+      articles: articles ? JSON.parse(articles) : [],
+      preferences: {
+        current: currentPreferences ? JSON.parse(currentPreferences) : null,
+        next: nextPreferences ? JSON.parse(nextPreferences) : null,
+      },
+    });
+  }, []);
+
+  return <>{JSON.stringify(data, null, 2)}</>;
+};
+
+export default Data;
