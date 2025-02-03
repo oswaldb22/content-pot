91705ede1975f1cd1e42afb5c7fb1fe49a49eb82
Oswald BLASSOU <oswaldblassou@yahoo.fr>
2025-02-03 22:51:22 +0100

    feat: added /data route to Data component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
+import Data from "./pages/Data";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/data" element={<Data />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
