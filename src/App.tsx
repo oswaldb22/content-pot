5aa65c3be93b5d1297abc571dd7edea89d347a4d
Oswald BLASSOU <oswaldblassou@yahoo.fr>
2025-02-05 02:29:53 +0100

    feat: add NotFound route for handling undefined paths

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
 import Data from "./pages/Data";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,7 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/data" element={<Data />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
