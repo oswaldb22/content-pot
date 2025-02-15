a13a1c8c0aada6535352ae9769db52b394669a13
Oswald BLASSOU <oswaldblassou@yahoo.fr>
2025-02-15 15:11:14 +0100

    feat: add MillionLint plugin for improved linting in development mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+import MillionLint from "@million/lint";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
+    mode === "development" && MillionLint.vite(),
   ].filter(Boolean),
   resolve: {
     alias: {
